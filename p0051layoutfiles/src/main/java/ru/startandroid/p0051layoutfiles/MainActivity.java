b107c2f6239a89b5272bb2d12b1241b7320cb8e6
Denis <denischirchir@gmail.com>
2020-09-23 21:23:52 +0300

    android lessons

---
+++ p0051layoutfiles/src/main/java/ru/startandroid/p0051layoutfiles/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.startandroid.p0051layoutfiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.myscreen);
+    }
+}
